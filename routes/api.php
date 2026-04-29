0d6ecebf297bb307a6216b7cb2e630aa3af808ed
Laura <ortizizquierdolaurakaria@gmail.com>
2026-04-29 16:24:39 -0500

    CambiosLaravel

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Admin\UsuarioController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\ProductoController;
 
@@ -12,3 +14,6 @@ use App\Http\Controllers\Admin\ProductoController;
 
 Route::get('/productos', [ProductoController::class, 'indexApi']);
 Route::post('/login', [AuthenticatedSessionController::class, 'loginApi']);
+Route::post('/register', [RegisteredUserController::class, 'registerApi']);
+Route::get('/users', [UsuarioController::class, 'indexApi']);
+Route::delete('/users/{id}', [UsuarioController::class, 'destroyApi']);
